0132cc344f360616a41b01841d0aff27e999761a
MD Biplop Hossain <developerbiplop@gmail.com>
2022-04-04 14:52:23 +0600

    Project setup and router setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav className='bg-gray-800 w-full text-white p-4 font-mono'>
+      <div className="container mx-auto flex flex-wrap justify-between items-center">
+        <h1 className='basis-1/4 lg:text-2xl md:text-2xl'>Ponno Analysis</h1>
+        <ul className='flex justify-end list-none text-base basis-3/4'>
+          <li> <Link className='transition duration-300 hover:bg-gray-700 p-4 rounded mr-3' to="/home">Home</Link> </li >
+          <li> <Link className='transition duration-300 hover:bg-gray-700 p-4 rounded mr-3' to="/reviews">Reviews</Link> </li>
+          <li> <Link className='transition duration-300 hover:bg-gray-700 p-4 rounded mr-3' to="/dashboard">Dashboard</Link> </li>
+          <li> <Link className='transition duration-300 hover:bg-gray-700 p-4 rounded mr-3' to="/blogs">Blogs</Link> </li>
+          <li> <Link className='transition duration-300 hover:bg-gray-700 p-4 rounded' to="/about">About</Link> </li>
+        </ul >
+      </div >
+    </nav >
+  );
+};
+
+export default Header;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <section className='py-7 font-mono'>
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className='text-center text-7xl text-blue-700 font-bold'>404</h1>
+        <h1 className='text-center text-7xl text-blue-700 font-bold'>Page Not Found</h1>
+      </div>
+    </section>
+  );
+};
+
+export default NotFound;
